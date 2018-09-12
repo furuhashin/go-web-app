9a98ef1d86c2783301902f8754173a7036514d44
nob <yasai.saita@gmail.com>
2018-09-12 23:09:24 +0900

    [done]2.6.2 認証プロパイダからのレスポンスの解釈まで

--- chat/main.go
+++ chat/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -26,11 +30,18 @@ func main() {
 	var addr = flag.String("addr", ":8080", "chat.html")
 	//フラグを解釈する
 	flag.Parse()
+	gomniauth.SetSecurityKey("aqwsedrftgyhujikol")
+	gomniauth.WithProviders(
+		facebook.New("592533754731-q9fv2ie1l2et9dqkk9d59htoq8fhjkrm.apps.googleusercontent.com", "OUx2g-PSg5j0bxNBA4DlTZfn", "http://localhost:8080/auth/callback/facebook"),
+		github.New("592533754731-q9fv2ie1l2et9dqkk9d59htoq8fhjkrm.apps.googleusercontent.com", "OUx2g-PSg5j0bxNBA4DlTZfn", "http://localhost:8080/auth/callback/github"),
+		google.New("592533754731-q9fv2ie1l2et9dqkk9d59htoq8fhjkrm.apps.googleusercontent.com", "OUx2g-PSg5j0bxNBA4DlTZfn", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	//なんでnewRoom内でtracerを作成しないの？デフォルトのtracerを設定するため
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	//チャットルームの開始
 	go r.run()
