09dc6503cdce4045691419700a3315dd8a33de7a
nob <yasai.saita@gmail.com>
2018-10-22 23:06:39 +0900

    [done]3.2.1.1 認証サービスを使ったAvatarの実装 ミスっていたので修正

--- chat/avatar.go
+++ chat/avatar.go
@@ -5,5 +5,18 @@ import "github.com/pkg/errors"
 var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得することができません。")
 
 type Avatar interface {
-	GetAvatorURL(c *client) (string, error)
+	GetAvatarURL(c *client) (string, error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
 }
