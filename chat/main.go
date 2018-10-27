cbf2e8adadb341fc6e59fefc89581a50b9c8fd35
nob <yasai.saita@gmail.com>
2018-10-27 19:48:40 +0900

    [done]3.3.4 画像の提供

--- chat/main.go
+++ chat/main.go
@@ -63,6 +63,7 @@ func main() {
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars"))))
 	//チャットルームの開始
 	go r.run()
 	//webサーバを起動
