92af1124d32e6b2b23cb1db6b37896a84d23237a
nob <yasai.saita@gmail.com>
2018-11-05 21:08:43 +0900

    [done] 7.2　コードの中でデータを表現する

---
+++ meander/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"../../meander"
+	"encoding/json"
+	"net/http"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	//meander.APIKey = "TODO"
+	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+		respond(w, r, meander.Journeys)
+	})
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	//Encode()内部でenc.w.Write(b)をしているのでjsonの内容がレスポンスとして返る
+	return json.NewEncoder(w).Encode(data)
+}
